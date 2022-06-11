b4aef9a5227ccb59770675ed8cccaf972bfc8489
albi-art <albert.12vi@gmail.com>
2022-06-11 16:28:07 +0300

    Initial commit

---
+++ src/main/java/io/github/albertsongs/videoreceiversmanager/VideoReceiversManagerApplication.java
@@ -0,0 +1,13 @@
+package io.github.albertsongs.videoreceiversmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoReceiversManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VideoReceiversManagerApplication.class, args);
+    }
+
+}
---
+++ src/main/java/io/github/albertsongs/videoreceiversmanager/controller/v1/VideoController.java
@@ -0,0 +1,10 @@
+package io.github.albertsongs.videoreceiversmanager.controller.v1;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/receivers/{receiver_id}/playlists/{playlist_id}/videos")
+public final class VideoController {
+
+}
