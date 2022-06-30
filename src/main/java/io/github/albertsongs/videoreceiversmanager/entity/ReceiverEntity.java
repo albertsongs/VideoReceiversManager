91313a5c2dbee3ef6525dc3d3d9c354a89a52071
albi-art <albert.12vi@gmail.com>
2022-06-30 21:51:50 +0300

    removed unused ReceiverEntity ID generator

--- src/main/java/io/github/albertsongs/videoreceiversmanager/entity/ReceiverEntity.java
+++ src/main/java/io/github/albertsongs/videoreceiversmanager/entity/ReceiverEntity.java
@@ -13,7 +13,7 @@ public final class ReceiverEntity {
     @Id
     @GeneratedValue(generator = "hibernate-uuid")
     @GenericGenerator(name = "uuid", strategy = "uuid2")
-    private UUID id = UUID.randomUUID();
+    private UUID id;
     private String name;
     private String lastIpAddress;
     private Date updatedAt;
