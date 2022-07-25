76c3ccb159ffab9b4440f96887e34ff9a709dca4
albi-art <albert.12vi@gmail.com>
2022-07-25 21:07:56 +0300

    refactored+

--- src/main/java/io/github/albertsongs/videoreceiversmanager/entity/PlaylistEntity.java
+++ src/main/java/io/github/albertsongs/videoreceiversmanager/entity/PlaylistEntity.java
@@ -1,12 +1,14 @@
 package io.github.albertsongs.videoreceiversmanager.entity;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import java.util.List;
 
 @Entity(name = "playlist")
 @Data
+@NoArgsConstructor
 public final class PlaylistEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
